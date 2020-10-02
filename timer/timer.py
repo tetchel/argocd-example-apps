acb7047da9836bd0d3f5cfcf9133647d1172b1be
Tim Etchells <tetchell@redhat.com>
2020-10-02 14:28:47 -0400

    timer
    Signed-off-by: Tim Etchells <tetchell@redhat.com>

--- timer/timer.py
+++ timer/timer.py
@@ -4,14 +4,27 @@ import sys
 from datetime import datetime
 import time
 
-if (len(sys.argv) > 1):
-    count = int(sys.argv[1])
-else:
-    count = 10
-
-print('Count is {}'.format(count))
-
-for i in range(count):
+def print_time():
     now = datetime.now()
     print("It's {}".format(now.strftime('%d/%m/%Y %H:%M:%S')))
     time.sleep(1)
+
+INF = 'inf'
+
+if len(sys.argv) > 1:
+    count_arg = sys.argv[1]
+    if count_arg == INF or count_arg == '-1':
+        count = INF
+    else:
+        count = int(count_arg)
+else:
+    count = 10
+
+if count == INF:
+    print('Counting forever')
+    while True:
+        print_time()
+else:
+    print('Counting for {}s'.format(count))
+    for i in range(count):
+        print_time()
